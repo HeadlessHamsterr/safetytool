bbda137f294560c00581a5bcf4509486009ed611
HeadlessHamster <joost_rietdijk@live.nl>
2023-05-30 15:24:07 +0200

    Standaard donkere modus aangezet

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import Home from "./components/Home";
 import GenerateScreen from "./components/GenerateScreen";
 //import CalibrateScreen from './components/CalibrateScreen';
 import HomeIcon from "@mui/icons-material/Home";
-import { Alert, Snackbar, Slide } from "@mui/material";
+import { Alert, Snackbar, Slide, createTheme, ThemeProvider } from "@mui/material";
 import { useState, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 import "@fontsource/roboto/300.css";
@@ -13,6 +13,12 @@ import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
 const serverURL = "http://localhost:3001";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark'
+  }
+});
 
 function App() {
   const [safetyfunctions, setSafetyfunctions] = useState(null);
@@ -46,7 +52,7 @@ function App() {
     });
 
     return () => {
-      window.removeEventListener("beforeunload", () => {});
+      window.removeEventListener("beforeunload", () => { });
     };
   });
 
@@ -106,58 +112,60 @@ function App() {
   }
 
   return (
-    <div className="container">
-      <Snackbar
-        TransitionComponent={slideFromTop}
-        anchorOrigin={{ vertical: "top", horizontal: "center" }}
-        open={showSnackbar}
-        autoHideDuration={snackbarAutohide ? 5000 : null}
-        onClose={handleSnackbarClose}
-      >
-        <Alert
-          id="alertSnackbar"
-          severity={alertSeverity}
-          variant="filled"
+    <ThemeProvider theme={darkTheme}>
+      <div className="container">
+        <Snackbar
+          TransitionComponent={slideFromTop}
+          anchorOrigin={{ vertical: "top", horizontal: "center" }}
+          open={showSnackbar}
+          autoHideDuration={snackbarAutohide ? 5000 : null}
           onClose={handleSnackbarClose}
         >
-          {alertText}
-        </Alert>
-      </Snackbar>
-      <div className="pageTitle">
-        {homeHidden ? (
-          <HomeIcon
-            className="pageTitleIcon"
-            fontSize="35px"
-            id="homeBtn"
-            onClick={() => iconClick()}
-          />
-        ) : null}
-        {calibrateScreenHidden
-          ? "Van den Pol - Safetytool"
-          : "Nieuwe vragenlijst instellen"}
+          <Alert
+            id="alertSnackbar"
+            severity={alertSeverity}
+            variant="filled"
+            onClose={handleSnackbarClose}
+          >
+            {alertText}
+          </Alert>
+        </Snackbar>
+        <div className="pageTitle">
+          {homeHidden ? (
+            <HomeIcon
+              className="pageTitleIcon"
+              fontSize="35px"
+              id="homeBtn"
+              onClick={() => iconClick()}
+            />
+          ) : null}
+          {calibrateScreenHidden
+            ? "Van den Pol - Safetytool"
+            : "Nieuwe vragenlijst instellen"}
+        </div>
+        <Home
+          returnSafetyfunctions={fileUploaded}
+          sessionId={sessionId}
+          hidden={homeHidden}
+          showSnackbar={(severity, error, autoHide) =>
+            enableSnackbar(severity, error, autoHide)
+          }
+          hideSnackbar={() => handleSnackbarClose("", "")}
+          serverURL={serverURL}
+        />
+        <GenerateScreen
+          safetyData={safetyfunctions}
+          hidden={generateScreenHidden}
+          returnHome={() => iconClick()}
+          sessionId={sessionId}
+          showSnackbar={(severity, error, autoHide) =>
+            enableSnackbar(severity, error, autoHide)
+          }
+          hideSnackbar={() => handleSnackbarClose("", "")}
+          serverURL={serverURL}
+        />
       </div>
-      <Home
-        returnSafetyfunctions={fileUploaded}
-        sessionId={sessionId}
-        hidden={homeHidden}
-        showSnackbar={(severity, error, autoHide) =>
-          enableSnackbar(severity, error, autoHide)
-        }
-        hideSnackbar={() => handleSnackbarClose("", "")}
-        serverURL={serverURL}
-      />
-      <GenerateScreen
-        safetyData={safetyfunctions}
-        hidden={generateScreenHidden}
-        returnHome={() => iconClick()}
-        sessionId={sessionId}
-        showSnackbar={(severity, error, autoHide) =>
-          enableSnackbar(severity, error, autoHide)
-        }
-        hideSnackbar={() => handleSnackbarClose("", "")}
-        serverURL={serverURL}
-      />
-    </div>
+    </ThemeProvider>
   );
 }
 //<CalibrateScreen hidden={calibrateScreenHidden} sessionId={sessionId}/>
